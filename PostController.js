5d2e1a6ec4c7dc6a062f40b7083ae27ed327f47d
alx8437 <dezion777@gmail.com>
2021-04-17 18:56:09 +0300

    added PostController.js for posts router

---
+++ PostController.js
@@ -0,0 +1,17 @@
+import Post from "./Post.js";
+
+class PostController {
+    async create(req, res) {
+        try {
+            const {author, title, content, picture} = req.body
+            console.log(author)
+            const post =  await Post.create({author, title, content, picture})
+            res.json(post)
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+}
+
+export default new PostController()
+
--- router.js
+++ router.js
@@ -1,19 +1,10 @@
 import Router from 'express'
-import Post from "./Post.js";
+import PostController from "./PostController.js";
+
 
 const router = new Router()
-console.log(router)
 
-router.post('/posts', async (req, res) => {
-    try {
-        const {author, title, content, picture} = req.body
-        const post =  await Post.create({author, title, content, picture})
-        res.json(post)
-    } catch (e) {
-        res.status(500).json(e)
-    }
-})
-
+router.post('/posts', PostController.create)
 router.get('posts')
 router.get('posts/:id')
 router.put('posts/:id')
